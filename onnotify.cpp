5d0eee58e244bd1e1341cbd922f87e098ce8b323
Hack012 <veetoveen@naver.com>
2019-07-22 16:47:16 +0900

    send

--- onnotify.cpp
+++ onnotify.cpp
@@ -1,5 +1,5 @@
-#include <eosio/eosio.hpp>
-#include <eosio/asset.hpp>
+#include<eosio/eosio.hpp>
+#include<eosio/asset.hpp>
 
 using namespace eosio;
 
@@ -9,41 +9,44 @@ using contract::contract;
 
 ACTION dummy() {}
 
-[[eosio::on_notify("eosio.token::transfer")]] //to: 보낼 사람, from: 보내는사람
+[[eosio::on_notify("eosio.token::transfer")]]
 void ontransfer(name from, name to, asset quantity, std::string memo) {
 if(from == get_self()) {
-outs myTable(get_self(), get_self().value); //출금액 확인하는 테이블
-if(myTable.begin() == myTable.end()) {
-myTable.emplace(from, [&](auto& row) {
+Receiver receiver(get_self(), get_self().value);
+auto itr = receiver.find(to.value);
+if(itr == receiver.end()) {
+receiver.emplace(from, [&](auto& row) {
+row.user = to;
 row.balance = quantity;
 });
 } else {
-auto itr = myTable.begin();
-myTable.modify(itr, from, [&](auto& row) {
+receiver.modify(itr, from, [&](auto& row) {
 row.balance += quantity;
 });
 }
-} else if(to == get_self()) { //입금액 확인하는 테이블 
-ins myTable(get_self(), get_self().value);
-if(myTable.begin() == myTable.end()) {
-    myTable.emplace(to, [&](auto& row) {
-        row.balance = quantity;
-    });
 } else {
-    auto itr = myTable.begin();
-    myTable.modify(itr, to, [&](auto& row) {
-        row.balance += quantity;
-    });
+Sender sender(get_self(), get_self().value);
+auto itr = sender.find(from.value);
+if(itr == sender.end()) {
+sender.emplace(to, [&](auto& row) {
+row.user = from;
+row.balance = quantity;
+});
+} else {
+sender.modify(itr, to, [&](auto& row) {
+row.balance += quantity;
+});
 }
 }
 }
 private:
-TABLE outstruct {
+TABLE trxlist {
+name user;
 asset balance;
 
-uint64_t primary_key() const { return balance.symbol.code().raw(); }
+uint64_t primary_key() const { return user.value; }
 };
 
-typedef multi_index<"out"_n, outstruct> outs;
-typedef multi_index<"in"_n, outstruct> ins; //테이블 하나를 만들어줬다.
+typedef multi_index<"eossender"_n, trxlist> Sender; //나한테 보냈다
+typedef multi_index<"eosreceiver"_n, trxlist> Receiver;//내가보낸 사람이 받았다!
 };
