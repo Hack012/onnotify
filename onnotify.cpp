b939b5b97b8c17bca3cdd12c95d2c18a97629e13
Hack012 <veetoveen@naver.com>
2019-07-22 14:25:38 +0900

    upgrade_code

--- onnotify.cpp
+++ onnotify.cpp
@@ -9,10 +9,10 @@ using contract::contract;
 
 ACTION dummy() {}
 
-[[eosio::on_notify("eosio.token::transfer")]]
+[[eosio::on_notify("eosio.token::transfer")]] //to: 보낼 사람, from: 보내는사람
 void ontransfer(name from, name to, asset quantity, std::string memo) {
 if(from == get_self()) {
-outs myTable(get_self(), get_self().value);
+outs myTable(get_self(), get_self().value); //출금액 확인하는 테이블
 if(myTable.begin() == myTable.end()) {
 myTable.emplace(from, [&](auto& row) {
 row.balance = quantity;
@@ -23,8 +23,18 @@ myTable.modify(itr, from, [&](auto& row) {
 row.balance += quantity;
 });
 }
+} else if(to == get_self()) { //입금액 확인하는 테이블 
+ins myTable(get_self(), get_self().value);
+if(myTable.begin() == myTable.end()) {
+    myTable.emplace(to, [&](auto& row) {
+        row.balance = quantity;
+    });
 } else {
-print("you are not from");
+    auto itr = myTable.begin();
+    myTable.modify(itr, to, [&](auto& row) {
+        row.balance += quantity;
+    });
+}
 }
 }
 private:
@@ -35,4 +45,5 @@ uint64_t primary_key() const { return balance.symbol.code().raw(); }
 };
 
 typedef multi_index<"out"_n, outstruct> outs;
+typedef multi_index<"in"_n, outstruct> ins; //테이블 하나를 만들어줬다.
 };
